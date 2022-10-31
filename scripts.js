8267fd62247b7bfea943689a8fdf0dc48ffa65d2
Jordane Anderson <jordaneanderson2@gmail.com>
2022-10-30 19:37:33 -0500

    Added CSS and Javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-/* Add your JavaScript to this file */
+function load() {
+	const btn = document.querySelector('button');
+    
+	btn.addEventListener("click", function(event){
+		event.preventDefault();
+		let email = document.querySelector('input').value;
+		if (email) {
+			document.querySelector('.message').innerHTML = 'Thank you! Your email address ' + email + ' has been added to our mailing list!';
+		}else {
+			document.querySelector('.message').innerHTML = 'Please enter a valid email address.'
+		}
+	});
+}
+window.addEventListener("load", load);
